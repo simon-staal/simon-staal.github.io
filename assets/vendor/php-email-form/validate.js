0a691175f3d2fec473606eef2be2771e6275942a
Simon Staal <sts219@ic.ac.uk>
2022-07-15 21:53:28 +0200

    Added projects, fixed form?

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -64,7 +64,7 @@
     })
     .then(data => {
       thisForm.querySelector('.loading').classList.remove('d-block');
-      if (data.trim() == 'OK') {
+      if (data.trim() == 'OK' || JSON.parse(data)["ok"] == true) {
         thisForm.querySelector('.sent-message').classList.add('d-block');
         thisForm.reset(); 
       } else {
